c90c74621d49e5acc52f63bb3cec5079343840d9
Eddi Sanchez <eddi@heyeddi.com>
2025-04-13 20:18:32 -0600

    Add get_current_timer method to Client class

--- clickup_apiV2/client.py
+++ clickup_apiV2/client.py
@@ -18,10 +18,10 @@ class Client:
             response = requests.get(url, headers=headers)
             # Raise an exception for HTTP errors
             response.raise_for_status()
-            
+
             # Parse the JSON response
             data = response.json()
-            
+
             # Return the team ID(s) from the response
             if response == "short":
                 team_ids = [team['id'] for team in data.get('teams', [])]
@@ -43,19 +43,19 @@ class Client:
             response = requests.get(url, headers=headers)
             # Raise an exception for HTTP errors
             response.raise_for_status()
-            
+
             # Parse the JSON response
             data = response.json()
-            
+
             # Extract and return the workspace (space) details
             if response == "short":
                 workspaces = [{"id": space["id"], "name": space["name"]} for space in data.get("spaces", [])]
                 return workspaces
-        
+
         except requests.exceptions.RequestException as e:
             print(f"An error occurred while fetching workspaces: {e}")
             return None
-    
+
     def get_workspace_folders(self, workspace_id):
         url = f"{self.server}/api/v2/space/{workspace_id}/folder"
         headers = {
@@ -219,7 +219,7 @@ class Client:
             return None
 
     def delete_task(self,task_id):
-        
+
         url = f"https://api.clickup.com/api/v2/task/{task_id}"
         headers = {
             "accept": "application/json",
@@ -257,7 +257,23 @@ class Client:
             print(f"An error occurred while setting custom field: {e}")
             return None
 
-
+    def get_current_timer(self):
+        url = "https://api.clickup.com/api/v2/team/time_entries/current"
+        headers = {
+            "accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": self.api_token
+        }
+        try:
+            # Make the PUT request with data in the JSON payload
+            response = requests.get(url,headers=headers)
+            # Raise an exception for HTTP errors
+            response.raise_for_status()
+            # Return the response data or success message
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred while setting custom field: {e}")
+            return None
 
 """
     def get_workspace_folders(self,workspace_id):
